61d3345faceec660ffc99954a028ad5da9c95725
Victor Makoski <vmakoski.training@innevo.cl>
2024-05-10 18:25:15 -0300

    fix  routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent, Page1Component, Page2Component } from './pages';
 
 export const routes: Routes = [
-  { path:"", component: HomeComponent },
+  { path:"home", component: HomeComponent },
   { path:"page-1", component: Page1Component },
-  { path:"page-2", component: Page2Component }
+  { path:"page-2", component: Page2Component },
+  { path: "**", redirectTo:"home" }
 ];
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -12,8 +12,8 @@ import { RouterLink } from '@angular/router';
 export class ToolbarComponent {
 
   routes = [
-    { name: "Home", path: "" },
-    { name: "Component 1", path: "/page-1" },
-    { name: "Component 2", path: "/page-2" }
+    { name: "Home", path: "home" },
+    { name: "Component 1", path: "page-1" },
+    { name: "Component 2", path: "page-2" }
   ]
 }
