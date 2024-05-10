ff9afca13a048a93f3eb3dec79d4423fb3bf296f
Victor Makoski <vmakoski.training@innevo.cl>
2024-05-10 18:36:16 -0300

    first signal

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
+interface Route {
+  name: string
+  path: string
+}
 @Component({
   selector: 'app-toolbar',
   standalone: true,
@@ -11,9 +15,16 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class ToolbarComponent {
 
-  routes = [
-    { name: "Home", path: "home" },
-    { name: "Component 1", path: "page-1" },
-    { name: "Component 2", path: "page-2" }
-  ]
+  // routes = [
+  //   { name: "Home", path: "home" },
+  //   { name: "Component 1", path: "page-1" },
+  //   { name: "Component 2", path: "page-2" }
+  // ]
+
+  // En el HTML la signal necesita () para leerse
+  routes = signal<Route[]>([
+      { name: "Home", path: "home" },
+      { name: "Component 1", path: "page-1" },
+      { name: "Component 2", path: "page-2" }
+    ])
 }
