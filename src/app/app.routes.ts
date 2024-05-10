b499899870456ff0713a0b3259da3758c310a934
Victor Makoski <vmakoski.training@innevo.cl>
2024-05-10 16:38:27 -0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent, Page1Component, Page2Component } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path:"", component: HomeComponent },
+  { path:"page-1", component: Page1Component },
+  { path:"page-2", component: Page2Component }
+];
