2cf89ecaab13e6e0d963fb8ff63a6c28f7e51215
Victor Makoski <vmakoski.training@innevo.cl>
2024-05-11 14:23:29 -0300

    vreate user-get component

---
+++ src/app/components/user-get/user-get.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-get',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-get.component.html',
+  styleUrl: './user-get.component.scss'
+})
+export class UserGetComponent {
+
+}
